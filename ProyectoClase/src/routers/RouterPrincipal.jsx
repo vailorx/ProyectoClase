3c0b800f67709417482f77cc9dd1faf3ac2d515c
vailorx <alvarorodrigoaj@gmail.com>
2023-05-31 19:31:40 +0200

    todo

--- ProyectoClase/src/routers/RouterPrincipal.jsx
+++ ProyectoClase/src/routers/RouterPrincipal.jsx
@@ -1,50 +1,23 @@
 import React from 'react'
-import {Routes, Route, NavLink, BrowserRouter, Navigate, useNavigate} from 'react-router-dom';
+import {Routes, Route, NavLink, BrowserRouter, Navigate} from 'react-router-dom';
 import {Inicio} from '../components/Inicio';
 import {Login} from '../components/Login';
 import {Registro} from '../components/Registro';
 import {Explorar} from '../components/Explorar';
+import {QueHacerHoy} from '../components/QueHacerHoy';
 export const RouterPrincipal = () => {
-    /*
-    {Futuro footer}
-     <footer className="footer p-10 bg-neutral text-neutral-content">
-  <div>
-    <span className="footer-title">Servicios</span> 
-    <a className="link link-hover">Diseño</a>
-    <a className="link link-hover">Marketing</a>
-  </div> 
-  <div>
-    <span className="footer-title">Compañia</span> 
-    <a className="link link-hover">Sobre nosotros</a>
-    <a className="link link-hover">Contacto</a>
-    <a className="link link-hover">Empleo</a>
-  </div> 
-  <div>
-    <span className="footer-title">Legal</span> 
-    <a className="link link-hover">Terminos de uso</a>
-    <a className="link link-hover">Politica de privacidad</a>
-    <a className="link link-hover">Politica de coockies</a>
-  </div>
-</footer>
-    */
-   /*
-   const navegar = useNavigate();
-   const enviar = (lugar) =>{
-    let url = "/" + {lugar};
-    navegar(url)
-   }
-   */
+
   return (
     <BrowserRouter>
     <div>
     <nav className="navbar bg-base-100">
   <div className="flex-1">
-    <NavLink className="btn btn-ghost normal-case text-xl" to="/">Callejeando</NavLink>
+    <NavLink className="text-xl normal-case btn btn-ghost" to="/">Callejeando</NavLink>
   </div>
   <div className="flex-none">
-    <ul className="menu menu-horizontal px-1">
+    <ul className="px-1 menu menu-horizontal">
       <li><NavLink to="/explorar">Explorar</NavLink></li>
-      <li tabIndex={0}>
+      {/*<li tabIndex={0}>
             <a>
           Login
           <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
@@ -53,8 +26,8 @@ export const RouterPrincipal = () => {
           <li><NavLink to="/login">Login</NavLink></li>
           <li><NavLink to="/registro">Registro</NavLink></li>
         </ul>
-      </li>
-      <li><a>Distancia</a></li>
+  </li>*/}
+      <li><NavLink to="/quehacerhoy">Que hacer hoy</NavLink></li>
     </ul>
   </div>
 </nav>
@@ -65,6 +38,7 @@ export const RouterPrincipal = () => {
             <Route path='/login' element={<Login />}/>
             <Route path='/registro' element={<Registro />}/>
             <Route path='/explorar' element={<Explorar />}/>
+            <Route path='/quehacerhoy' element={<QueHacerHoy />}/>
             <Route path='/redirigir' element={<Navigate to="/inicio" />} />
             <Route path='*' element={(
                 <>
